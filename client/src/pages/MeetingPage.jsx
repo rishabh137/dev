af832fcc68852009d03906bbddf9b0ec880e876a
Rishabh Raj <rishabhraj13951@gmail.com>
2025-07-03 22:48:33 +0530

    temp

--- client/src/pages/MeetingPage.jsx
+++ client/src/pages/MeetingPage.jsx
@@ -109,7 +109,7 @@ const MeetingPage = () => {
             <div className="flex flex-1">
                 <div className={`${codingStarted ? 'flex-wrap-reverse' : ''} w-full flex flex-col items-center justify-center`}>
                     <div className={`flex gap-4 mb-4 mt-5 ${codingStarted ? 'flex-col-reverse h-[0vh] mt-[665px]' : ''}`}>
-                        <video ref={localVideoRef} className={`w-[700px] rounded ${codingStarted && remoteConnected ? 'h-[245px]' : ''}`} playsInline muted></video>
+                        <video ref={localVideoRef} className={`rounded ${codingStarted && remoteConnected ? 'h-[245px]' : ''}`} playsInline muted></video>
                         <video
                             ref={remoteVideoRef}
                             className={`rounded ${remoteConnected ? '' : 'hidden'} ${codingStarted ? 'h-[400px]' : 'h-full w-[700px]'}`}
